cf6e75b1445b2d9ea9f3e8f8251f2deff71d8ae0
MangoS9 <brinexbz@gmail.com>
2024-10-15 20:38:59 +1100

    Deploy

--- src/component/Loading_SCR.tsx
+++ src/component/Loading_SCR.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import './style/Loading_SCR.scss';
 
 interface LoadingSCRProps {
--- src/component/Navigation.tsx
+++ src/component/Navigation.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './style//Navigation.scss'
 
 function Navigation(){
